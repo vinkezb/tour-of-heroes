60b2b8531dc3e0df4b9dea6fabcad50323bc243e
vinkezb <kevin.bra99@gmail.com>
2019-04-02 09:59:22 +0200

    hero and message service

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
   
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-
+  
   OnSelect(hero:Hero): void{
     this.selectedHero = hero;
   }
-
-  constructor() { }
+  getHeroes():void {
+     this.heroService.getHeroes()
+     .subscribe(heroes => this.heroes = heroes);
+  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
